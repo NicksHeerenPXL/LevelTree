b6381f8e87d6b412a2661a8abc71084a941fc654
NicksHeerenPXL <nick.sheeren@student.pxl.be>
2021-12-16 13:31:40 +0100

    Zoomfunctionaliteiten toegevoegd

--- LevelTree/Js/Main.js
+++ LevelTree/Js/Main.js
@@ -1,1 +1,38 @@
 "use Strict"
+document.getElementById('tooltipLv1').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv1');
+    zoomElement.style.zoom='92%';
+})
+document.getElementById('tooltipLv2').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv2');
+    zoomElement.style.zoom='92%';
+})
+document.getElementById('tooltipLv3').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv3');
+    zoomElement.style.zoom='95%';
+})
+document.getElementById('tooltipLv4').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv4');
+    zoomElement.style.zoom='95%';
+})
+document.getElementById('tooltipLv5').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv5');
+    zoomElement.style.zoom='95%';
+})
+document.getElementById('tooltipLv6').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv6');
+    zoomElement.style.zoom='95%';
+})
+document.getElementById('tooltipLv7').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv7');
+    zoomElement.style.zoom='95%';
+})
+document.getElementById('tooltipLv8').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv8');
+    zoomElement.style.zoom='95%';
+})
+document.getElementById('tooltipLv9').addEventListener("click", function () {
+    let zoomElement = document.getElementById('tooltipLv9');
+    zoomElement.style.zoom='95%';
+})
+
